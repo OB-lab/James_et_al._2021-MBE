3cf440140e709335fab691a3f6f46a46f293812e
Henry Arenas-Castro <30415334+henryarenasc@users.noreply.github.com>
2018-12-20 11:40:57 +1000

    Update getpopmap.pl

--- getpopmap.pl
+++ getpopmap.pl
@@ -1,34 +1,55 @@
-# getpopmap.pl
-# 27/11/2018
-# henry.arenasc@gmail.com
-# Generte tab-delimited population map file from a CVF file.
-# Use: per getpopmap.pl (0)vcf_file (1)output_filename (2)root_name_pop1 (3)root_name_pop2
+# getpopmap.pl generates a tab-delimited population specification file from a CVF file 
+# containing either two or three populations
+
+# Use: perl getpopmap.pl (0)input_vcf_file (1)output_popmap_file (2)number_of_pops 
+#      (3)name_pop1 (4)name_pop2 (5)name_pop3
+
+# Assumptions: 
+# - Sample names are composed by PopName-SampleID_PopName-SampleID (i.e. D12-291_D12-291)
+# - The header line containing the sample names is tab-delimitated and stats by #CHROM
+# - Sample names start at column number 10
 
 use warnings;
 use strict;
 
-open (CVF, $ARGV[0]) or die;
-open (OUT, ">$ARGV[1]") or die;
+open (VcF, $ARGV[0]) or die; # Load the input VCF file
+open (OUT, ">$ARGV[1]") or die; # Create the output file
 
-my $c=0;
+# Read the VCF file line per line until reaching the header and extracting the sample and population names
+my $c=0; # Keep reading the file until finding the header
 while ($c==0) {
-	my $l = <CVF>;
+	my $l = <VCF>; # Read the line
 	chomp $l;
 	my @col = split(/\t/, $l);
-	if ($col[0] eq "#CHROM") {
-		for (my $i=9; $i<scalar @col; $i++) { #Assuming that pop names start at column #10 in the VCF file
-			my @sn = split (/-/, $col[$i]);
-			if ($sn[0] eq $ARGV[2]) {
-				print OUT "$col[$i]\t$ARGV[2]\n";
+	if ($col[0] eq "#CHROM") { # Check whether the current line corresponds to the header
+		for (my $i=9; $i<scalar @col; $i++) { # Read sample names from header's column number 10 onwards
+			my @sn = split (/-/, $col[$i]); # Extract population name from the sample name
+			# If there are two populations, write the sample and population names in a new line in the output file...
+			if ($ARGV[2] == 2) {
+				if ($sn[0] eq $ARGV[3]) { # if the sample belongs to population 1
+					print OUT "$col[$i]\t$ARGV[3]\n"; 
+				}
+				if ($sn[0] eq $ARGV[4]) { # if the sample belongs to population 2
+					print OUT "$col[$i]\t$ARGV[4]\n";
+				}
 			}
-			if ($sn[0] eq $ARGV[3]) {
-				print OUT "$col[$i]\t$ARGV[3]\n";
-			}
+			# If there are three populations, write the sample and population names in a new line in the output file...
+			if ($ARGV[2] == 3) { 
+				if ($sn[0] eq $ARGV[3]) { # if the sample belongs to population 1
+					print OUT "$col[$i]\t$ARGV[3]\n"; 
+				}
+				if ($sn[0] eq $ARGV[4]) { # if the sample belongs to population 2
+					print OUT "$col[$i]\t$ARGV[4]\n";
+				}
+				if ($sn[0] eq $ARGV[5]) { # if the sample belongs to population 3
+					print OUT "$col[$i]\t$ARGV[5]\n";
+				}
+			}	
 		}
-		$c = 1;
+		$c = 1; # Stop reading the file, header's useful information was already taken
 		}
 }
 
-close CVF;
+close VCF;
 close OUT;
 exit;
