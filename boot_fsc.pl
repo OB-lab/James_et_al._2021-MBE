fff07a1ee8c6058dd4e5c1aa6c8631a247c0de65
Henry Arenas-Castro <30415334+henryarenasc@users.noreply.github.com>
2019-01-12 13:46:09 +1000

    Update and rename boot_fsc.pl to runboot1.pl

--- boot_fsc.pl
+++
@@ -1,6 +0,0 @@
-# boot_fsc.pl perform parametric bootstrap based on the results of the best run of the best demographic model
-
-# Use: perl boot_fsc.pl (0)best_model (1)best_run (2)number_of_replicates (3)root_name
-
-
-
