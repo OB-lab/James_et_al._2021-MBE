489acc098688206b632285b4c4e609a6b411dbb5
Henry Arenas-Castro <30415334+henryarenasc@users.noreply.github.com>
2019-01-12 10:35:21 +1000

    Create boot_fsc.pl

---
+++ boot_fsc.pl
@@ -0,0 +1,6 @@
+# boot_fsc.pl perform parametric bootstrap based on the results of the best run of the best demographic model
+
+# Use: perl boot_fsc.pl (0)best_model (1)best_run (2)number_of_replicates (3)root_name
+
+
+
