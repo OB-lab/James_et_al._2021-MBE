41a1b603dd2f8421e4cea6f79056b30355917e5f
Henry Arenas-Castro <30415334+henryarenasc@users.noreply.github.com>
2019-01-18 16:44:58 +1000

    Update runboot1.pl

--- runboot1.pl
+++ runboot1.pl
@@ -1,14 +1,17 @@
-# runboot1.pl perform parametric bootstrap based on the results of the best run of the best demographic model
+# runboot1.pl simulates SFS files based on an given demographic model for performing parametric bootstrap
 
 # Use: perl boot_fsc.pl (0)root_name (1)number_of_replicates
+
+# Assumptions: 
+# - The parameter (*.par), initial parameter values (*.pv), template (*.tpl), and estimation (*.est) files should be contained in the local directory
 
 use warnings;
 use strict;
 
-# Run fsc to simulate the specified number of SFS from the *.par file
+# Run fsc to simulate the specified number of SFS from a parameter file
 system ("/home/uqharena/fsc26_linux64/fsc26 -i $ARGV[0].par -n$ARGV[1] -j -m -s0 -x -I -q");
 
-# Copy the *.tpl, *.est, and *.pv files to the replicate directories containing the simulated SFS
+# Copy the initial parameter values, template, and estimation files to the newly created directories containing the simulated SFS
 my $rep = 1;
 while ($rep <= $ARGV[1]) {
   system ("cp $ARGV[0].tpl $ARGV[0]/$ARGV[0]_$rep/");
