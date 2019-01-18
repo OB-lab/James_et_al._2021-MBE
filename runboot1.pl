0e95b90f69b8bc40c4512379d7950314bfbab400
Henry Arenas-Castro <30415334+henryarenasc@users.noreply.github.com>
2019-01-18 12:51:17 +1000

    Update runboot1.pl

--- runboot1.pl
+++ runboot1.pl
@@ -1,4 +1,4 @@
-# boot_fsc.pl perform parametric bootstrap based on the results of the best run of the best demographic model
+# runboot1.pl perform parametric bootstrap based on the results of the best run of the best demographic model
 
 # Use: perl boot_fsc.pl (0)root_name (1)number_of_replicates
 
@@ -17,19 +17,5 @@ while ($rep <= $ARGV[1]) {
   $rep += 1; # Move to the next replicate directory
 }
 
-# Create a bash file to run in parallel fsc in every directory
-open (OUT, ">runboot_$ARGV[0].sh") or die;
-print OUT "#Run boostrap for $ARGV[0]\n
-#PBS -A UQ-SCI-BiolSci\n
-#PBS -l select=1:ncpus=9:mem=1GB\n
-#PBS -l walltime=1:00:00\n
-#PBS -J 1-$ARGV[1]\n
-\n
-cd /30days/uqharena/bootstrap/$ARGV[0]/$ARGV[0]/$ARGV[0]_\${PBS_ARRAY_INDEX}/; /home/uqharena/fsc26_linux64/fsc26 -t $ARGV[0].tpl -e $ARGV[0].est --initValues $ARGV[0].pv -n100000 -m -L30 -M -c10";
-
-# Launch the job
-system ("qsub runboot_$ARGV[0].sh");
-
-close OUT;
 exit;
 
